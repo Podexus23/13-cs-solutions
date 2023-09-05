5cd1ca8b4f36e475f91f503c8c1f41dd43e334c7
Podexus23 <56443118+Podexus23@users.noreply.github.com>
2023-09-05 17:16:16 +0300

    Update arrayList.js

--- arrayList.js
+++ arrayList.js
@@ -1,34 +1,31 @@
 class ArrayList {
   constructor() {
-    this.length = 0;
     this.data = {};
+    this.length = 0;
   }
-  push(value) {
-    this.data[this.length] = value;
+
+  push(num) {
+    this.data[this.length] = num;
     this.length++;
   }
+
   pop() {
-    const ans = this.data[this.length - 1];
+    const res = this.data[this.length - 1];
     delete this.data[this.length - 1];
     this.length--;
-    return ans;
+    return res;
   }
+
   get(index) {
     return this.data[index];
   }
+
   delete(index) {
-    const ans = this.data[index];
-    this._replaceData(index);
-    return ans;
-  }
-  _replaceData(index) {
+    if (index > this.length) return;
     for (let i = index; i < this.length; i++) {
       this.data[i] = this.data[i + 1];
     }
     delete this.data[this.length - 1];
     this.length--;
-  }
-  serialize() {
-    return this.data;
   }
 }
